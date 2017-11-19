0f8ceedf4ae3737aee88eaf901786cb60c5e9afd
MRbie <1748741328@qq.com>
2017-11-19 16:24:08 +0800

    Java线程的两种实现方法

---
+++ src/com/bie/lesson01/thread/ThreadImplementRunnable.java
@@ -0,0 +1,46 @@
+package com.bie.lesson01.thread;
+
+import java.util.Random;
+
+/** 
+* @author  Author:别先生 
+* @date Date:2017年11月19日 下午4:18:38 
+*
+*
+*/
+public class ThreadImplementRunnable implements Runnable{
+
+	private String flag;
+	public ThreadImplementRunnable(String flag) {
+		this.flag = flag;
+	}
+	
+	@Override
+	public void run() {
+		//得到当前线程的名称
+		String tName = Thread.currentThread().getName();
+		System.out.println(tName + " 线程的run方法被调用......");
+		Random random = new Random();
+		for(int i=0; i<20; i++){
+			try {
+				Thread.sleep(random.nextInt(10) * 100);
+				System.out.println(tName + "......" + flag);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Thread thread1 = new Thread(new ThreadExtendThread("线程一"),"线程一");
+		Thread thread2 = new Thread(new ThreadExtendThread("线程二"),"线程二");
+		
+		//启动线程
+		thread1.start();
+		thread2.start();
+		//如果是调用thread的run方法，则只是一个普通的方法调用，不会开启新的线程
+		//thread1.run();
+		//thread2.run();
+	}
+	
+}
